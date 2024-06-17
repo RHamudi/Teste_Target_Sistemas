de2b3123a1ffc61a5d24582b156b52ab80f3090f
RHamudi <ramonramos.silva16@gmail.com>
2024-06-17 17:18:26 -0300

    Desafio 15 resolvido

---
+++ Questão numero 15/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Limites inferior e superior
+int inicio = 100;
+int fim = 200;
+
+// Fórmula para somatória de ímpares em um intervalo
+int soma = (fim - inicio + 1) * (inicio + fim) / 2;
+
+// Verificação se o limite inferior é ímpar
+if (inicio % 2 == 0) {
+    soma--; // Ajusta a soma se o limite inferior for par
+}
+
+// Apresentação do resultado
+Console.WriteLine($"Soma dos números ímpares entre {inicio} e {fim}: {soma}");
+    
