ec1f8f3115405c22d1a88c5b207b36cdbc5482d5
Ramon Ramos <94305379+RHamudi@users.noreply.github.com>
2024-06-17 16:34:20 -0300

    questão numero 10

---
+++ Questão numero 10/Program.cs
@@ -0,0 +1,15 @@
+﻿static int SomarEMultiplicar(int numero1, int numero2, out int produto)
+{
+    int soma = numero1 + numero2;
+    produto = numero1 * numero2;
+    return soma;
+}
+
+int num1 = 10;
+int num2 = 5;
+int soma, produto;
+
+soma = SomarEMultiplicar(num1, num2, out produto);
+
+Console.WriteLine("Soma: " + soma);
+Console.WriteLine("Produto: " + produto);
